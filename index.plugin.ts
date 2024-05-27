de10d056f915e3079d66daa08fdbbaa302a9e6b4
erkelost <1256029807@qq.com>
2024-05-27 11:37:11 +0800

    chore: update config

--- index.plugin.ts
+++ index.plugin.ts
@@ -3,44 +3,48 @@ import type { Config } from "@farmfe/core/binding";
 export default function NestPlugin(options?: Config['config']) {
   // not support multiple input files in nestjs we just need to use one input file first one
   const inputFileEntry = Object.values(options?.input || {})[0] ?? 'src/main.ts';
-  
+
   return {
     name: 'NestPlugin',
-    config: (_config) => ({
-      compilation: {
-        input: {
-          'NestJs': inputFileEntry,
-        },
-        script: {
-          plugins: [],
-          target: 'es2019',
-          parser: {
-            tsConfig: {
-              decorators: true,
-              dts: false,
-              noEarlyErrors: false,
-              tsx: false,
+    config: (config) => {
+      const mode = config.compilation.mode ?? process.env.NODE_ENV ?? 'development';
+      console.log(mode);
+      const isDev = mode === 'development';
+      return {
+        compilation: {
+          input: {
+            'NestJs': inputFileEntry,
+          },
+          script: {
+            plugins: [],
+            target: 'es2019',
+            parser: {
+              tsConfig: {
+                decorators: true,
+                dts: false,
+                noEarlyErrors: false,
+                tsx: false,
+              },
+            },
+            decorators: {
+              legacyDecorator: true,
+              decoratorMetadata: true,
+              decoratorVersion: '2021-12',
+              includes: [inputFileEntry],
+              excludes: ['node_modules/**/*'],
             },
           },
-          decorators: {
-            legacyDecorator: true,
-            decoratorMetadata: true,
-            decoratorVersion: '2021-12',
-            includes: [inputFileEntry],
-            excludes: ['node_modules/**/*'],
+          presetEnv: !isDev,
+          minify: !isDev,
+          output: {
+            format: 'esm',
+            targetEnv: 'node',
+            entryFilename: '[entryName].js',
+            filename: '[name].[hash].mjs',
           },
-        },
-        presetEnv: false,
-        treeShaking: false,
-        minify: false,
-        output: {
-          format: 'esm',
-          targetEnv: 'node',
-          entryFilename: '[entryName].js',
-          filename: '[name].[hash].mjs',
-        },
-        ...options
+          ...options
+        }
       }
-    })
+    }
   };
 }
--- src/b.controller.ts
+++ src/b.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class BController {
   @Get()
   findAll(): string {
-    return 'This action returns all users 123asdasasdasdasdasdasdasdasda4564 456 45646 4564 45646 12312313';
+    return 'This action returns all users';
   }
 }
